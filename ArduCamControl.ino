24209c31421a06ea8c0e0a7f522585caed28526f
electroJo <96352550+T-electroJo@users.noreply.github.com>
2023-10-19 16:25:38 +0200

    Update ArduCamControl.ino

--- ArduCamControl.ino
+++ ArduCamControl.ino
@@ -8,13 +8,14 @@ const int servo_pan_pin = 2;
 const int servo_tilt_pin = 3;
 const int ir_pin = 4;
 
-int default_pan_pos = 90;
-int default_tilt_pos = 90;
-int pan_pos = default_pan_pos;
-int tilt_pos = default_tilt_pos;
+int default_pan_pos = 0;
+int default_tilt_pos = 0;
+int pan_pos;
+int tilt_pos;
+int old_pan_pos;
+int old_tilt_pos;
 const int tilt_max = 150;
 
-bool status = false;
 bool highspeed = false;
 bool writing_mode = false;
 bool delete_mode = false;
@@ -31,13 +32,28 @@ decode_results results;
 String result;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Setup begin...");
   irrecv.enableIRIn(); // Enable IR Input
   pan.attach(servo_pan_pin); // Enable Servo for Pan
   tilt.attach(servo_tilt_pin); // Enable Servo for Tilt
+
+  int pan_value = EEPROM.read(8);
+  int tilt_value = EEPROM.read(9);
+  pan_pos = pan_value == 255 ? default_pan_pos : pan_value;
+  tilt_pos = tilt_value == 255 ? default_tilt_pos : tilt_value;
   move_servos(); // Apply movement
+
   if (checkFrequencies) {
     Serial.begin(9600); // Open Serial monitor if configure mode is enabled
   }
+
+  int default_disable = EEPROM.read(0);
+  if (default_disable != 0 || default_disable != 1) {
+    default_disable = 0;
+    EEPROM.write(0, default_disable);
+  }
+  Serial.println("Setup done");
 }
 
 void loop() {
@@ -45,7 +61,7 @@ void loop() {
     result = String(results.value, HEX).substring(2); // Take the last 2 digits from the HEX-String of the IR code.
     
     if (result == remote_UP) {
-      write_delete_check()
+      write_delete_check();
       if (tilt_pos < tilt_max) {
         tilt_pos++;
         if (highspeed && tilt_pos < tilt_max-4) {
@@ -54,7 +70,7 @@ void loop() {
       }
     }
     else if (result == remote_DOWN) {
-      write_delete_check()
+      write_delete_check();
       if (tilt_pos > 0) {
         tilt_pos--;
         if (highspeed && tilt_pos > 4) {
@@ -63,7 +79,7 @@ void loop() {
       }
     }
     else if (result == remote_LEFT) {
-      write_delete_check()
+      write_delete_check();
       if (pan_pos < 180) {
         pan_pos++;
         if (highspeed && pan_pos < 176) {
@@ -72,7 +88,7 @@ void loop() {
       }
     }
     else if (result == remote_RIGHT) {
-      write_delete_check()
+      write_delete_check();
       if (pan_pos > 0) {
         pan_pos--;
         if (highspeed && pan_pos > 4) {
@@ -81,23 +97,25 @@ void loop() {
       }
     }
     else if (result == remote_HOME) {
-      write_delete_check()
+      write_delete_check();
       pan_pos = default_pan_pos;
       tilt_pos = default_tilt_pos;
     }
     else if (result == remote_SPEED) {
-      write_delete_check()
+      write_delete_check();
       highspeed = !highspeed;
       delay(500);
     }
     else if (result == remote_DISABLE) {
-      write_delete_check()
-      if (pan.attached() || tilt.attached()) {
+      write_delete_check();
+      if (EEPROM.read(5) == 1) {
         pan.detach();
         tilt.detach();
+        EEPROM.write(5, 0);
       } else {
         pan.attach(servo_pan_pin);
         tilt.attach(servo_tilt_pin);
+        EEPROM.write(5, 1);
       }
       delay(500);
     }
@@ -108,99 +126,23 @@ void loop() {
     }
     else if (result == remote_PROGRAM) {
       writing_mode = !writing_mode;
+      Serial.print("Writing mode ");
+      Serial.println(writing_mode ? "enabled" : "disabled");
       delay(500);
     }
-    else if (result == remote_ZERO) {
-      if (writing_mode && delete_mode) {
-        EEPROM.write(0, 255);
-        EEPROM.write(1, 255);
-        writing_mode = false;
-        delete_mode = false;
-      }
-      else if (writing_mode) {
-        EEPROM.write(0, pan_pos);
-        EEPROM.write(1, tilt_pos);
-        writing_mode = false;
-      } else {
-        byte pan_value = EEPROM.read(0);
-        byte tilt_value = EEPROM.read(1);
-        if (pan_value <= 180 && tilt_value <= tilt_max) {
-          pan_pos = pan_value;
-          tilt_pos = tilt_value;
-        }
-      }
-    }
-    else if (result == remote_ONE) {
-      if (writing_mode && delete_mode) {
-        EEPROM.write(2, 255);
-        EEPROM.write(3, 255);
-        writing_mode = false;
-        delete_mode = false;
-      }
-      else if (writing_mode) {
-        EEPROM.write(2, pan_pos);
-        EEPROM.write(3, tilt_pos);
-        writing_mode = false;
-      } else {
-        byte pan_value = EEPROM.read(2);
-        byte tilt_value = EEPROM.read(3);
-        if (pan_value <= 180 && tilt_value <= tilt_max) {
-          pan_pos = pan_value;
-          tilt_pos = tilt_value;
-        }
-      }
-    }
-    else if (result == remote_TWO) {
-      if (writing_mode && delete_mode) {
-        EEPROM.write(4, 255);
-        EEPROM.write(5, 255);
-        writing_mode = false;
-        delete_mode = false;
-      }
-      else if (writing_mode) {
-        EEPROM.write(4, pan_pos);
-        EEPROM.write(5, tilt_pos);
-        writing_mode = false;
-      } else {
-        byte pan_value = EEPROM.read(4);
-        byte tilt_value = EEPROM.read(5);
-        if (pan_value <= 180 && tilt_value <= tilt_max) {
-          pan_pos = pan_value;
-          tilt_pos = tilt_value;
-        }
-      }
-    }
-    else if (result == remote_THREE) {
-      if (writing_mode && delete_mode) {
-        EEPROM.write(6, 255);
-        EEPROM.write(7, 255);
-        writing_mode = false;
-        delete_mode = false;
-      }
-      else if (writing_mode) {
-        EEPROM.write(6, pan_pos);
-        EEPROM.write(7, tilt_pos);
-        writing_mode = false;
-      } else {
-        byte pan_value = EEPROM.read(6);
-        byte tilt_value = EEPROM.read(7);
-        if (pan_value <= 180 && tilt_value <= tilt_max) {
-          pan_pos = pan_value;
-          tilt_pos = tilt_value;
-        }
-      }
-    }
-    else if (result == remote_FOUR) {
+    else if (result == remote_OK) {
       if (writing_mode && delete_mode) {
         EEPROM.write(8, 255);
         EEPROM.write(9, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(8, pan_pos);
         EEPROM.write(9, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(8);
         byte tilt_value = EEPROM.read(9);
@@ -210,17 +152,19 @@ void loop() {
         }
       }
     }
-    else if (result == remote_FIVE) {
+    else if (result == remote_ZERO) {
       if (writing_mode && delete_mode) {
         EEPROM.write(10, 255);
         EEPROM.write(11, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(10, pan_pos);
         EEPROM.write(11, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(10);
         byte tilt_value = EEPROM.read(11);
@@ -230,17 +174,19 @@ void loop() {
         }
       }
     }
-    else if (result == remote_SIX) {
+    else if (result == remote_ONE) {
       if (writing_mode && delete_mode) {
         EEPROM.write(12, 255);
         EEPROM.write(13, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(12, pan_pos);
         EEPROM.write(13, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(12);
         byte tilt_value = EEPROM.read(13);
@@ -250,17 +196,19 @@ void loop() {
         }
       }
     }
-    else if (result == remote_SEVEN) {
+    else if (result == remote_TWO) {
       if (writing_mode && delete_mode) {
         EEPROM.write(14, 255);
         EEPROM.write(15, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(14, pan_pos);
         EEPROM.write(15, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(14);
         byte tilt_value = EEPROM.read(15);
@@ -270,17 +218,19 @@ void loop() {
         }
       }
     }
-    else if (result == remote_EIGHT) {
+    else if (result == remote_THREE) {
       if (writing_mode && delete_mode) {
         EEPROM.write(16, 255);
         EEPROM.write(17, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(16, pan_pos);
         EEPROM.write(17, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(16);
         byte tilt_value = EEPROM.read(17);
@@ -290,20 +240,132 @@ void loop() {
         }
       }
     }
-    else if (result == remote_NINE) {
+    else if (result == remote_FOUR) {
       if (writing_mode && delete_mode) {
         EEPROM.write(18, 255);
         EEPROM.write(19, 255);
         writing_mode = false;
         delete_mode = false;
+        delay(500);
       }
       else if (writing_mode) {
         EEPROM.write(18, pan_pos);
         EEPROM.write(19, tilt_pos);
         writing_mode = false;
+        delay(500);
       } else {
         byte pan_value = EEPROM.read(18);
         byte tilt_value = EEPROM.read(19);
+        if (pan_value <= 180 && tilt_value <= tilt_max) {
+          pan_pos = pan_value;
+          tilt_pos = tilt_value;
+        }
+      }
+    }
+    else if (result == remote_FIVE) {
+      if (writing_mode && delete_mode) {
+        EEPROM.write(20, 255);
+        EEPROM.write(21, 255);
+        writing_mode = false;
+        delete_mode = false;
+        delay(500);
+      }
+      else if (writing_mode) {
+        EEPROM.write(20, pan_pos);
+        EEPROM.write(21, tilt_pos);
+        writing_mode = false;
+        delay(500);
+      } else {
+        byte pan_value = EEPROM.read(20);
+        byte tilt_value = EEPROM.read(21);
+        if (pan_value <= 180 && tilt_value <= tilt_max) {
+          pan_pos = pan_value;
+          tilt_pos = tilt_value;
+        }
+      }
+    }
+    else if (result == remote_SIX) {
+      if (writing_mode && delete_mode) {
+        EEPROM.write(22, 255);
+        EEPROM.write(23, 255);
+        writing_mode = false;
+        delete_mode = false;
+        delay(500);
+      }
+      else if (writing_mode) {
+        EEPROM.write(22, pan_pos);
+        EEPROM.write(23, tilt_pos);
+        writing_mode = false;
+        delay(500);
+      } else {
+        byte pan_value = EEPROM.read(22);
+        byte tilt_value = EEPROM.read(23);
+        if (pan_value <= 180 && tilt_value <= tilt_max) {
+          pan_pos = pan_value;
+          tilt_pos = tilt_value;
+        }
+      }
+    }
+    else if (result == remote_SEVEN) {
+      if (writing_mode && delete_mode) {
+        EEPROM.write(24, 255);
+        EEPROM.write(25, 255);
+        writing_mode = false;
+        delete_mode = false;
+        delay(500);
+      }
+      else if (writing_mode) {
+        EEPROM.write(24, pan_pos);
+        EEPROM.write(25, tilt_pos);
+        writing_mode = false;
+        delay(500);
+      } else {
+        byte pan_value = EEPROM.read(24);
+        byte tilt_value = EEPROM.read(25);
+        if (pan_value <= 180 && tilt_value <= tilt_max) {
+          pan_pos = pan_value;
+          tilt_pos = tilt_value;
+        }
+      }
+    }
+    else if (result == remote_EIGHT) {
+      if (writing_mode && delete_mode) {
+        EEPROM.write(26, 255);
+        EEPROM.write(27, 255);
+        writing_mode = false;
+        delete_mode = false;
+        delay(500);
+      }
+      else if (writing_mode) {
+        EEPROM.write(26, pan_pos);
+        EEPROM.write(27, tilt_pos);
+        writing_mode = false;
+        delay(500);
+      } else {
+        byte pan_value = EEPROM.read(26);
+        byte tilt_value = EEPROM.read(27);
+        if (pan_value <= 180 && tilt_value <= tilt_max) {
+          pan_pos = pan_value;
+          tilt_pos = tilt_value;
+        }
+      }
+    }
+    else if (result == remote_NINE) {
+      if (writing_mode && delete_mode) {
+        EEPROM.write(28, 255);
+        EEPROM.write(29, 255);
+        writing_mode = false;
+        delete_mode = false;
+        delay(500);
+      }
+      else if (writing_mode) {
+        EEPROM.write(28, pan_pos);
+        EEPROM.write(29, tilt_pos);
+        writing_mode = false;
+        delay(500);
+      } else {
+        byte pan_value = EEPROM.read(28);
+        byte tilt_value = EEPROM.read(29);
         if (pan_value <= 180 && tilt_value <= tilt_max) {
           pan_pos = pan_value;
           tilt_pos = tilt_value;
@@ -316,7 +378,7 @@ void loop() {
 
     move_servos();
     irrecv.resume();
-  } else {
+  } else if (checkFrequencies) {
     if (irrecv.decode(&results)) {
       result = String(results.value, HEX).substring(2);
       Serial.println(result);
@@ -325,11 +387,31 @@ void loop() {
 }
 
 void move_servos() {
-  if (pan.attached()) {
-    pan.write(pan_pos);
+  if (pan_pos > 180) {
+    Serial.print("Pan overdrive: ");
+    Serial.println(pan_pos);
+    pan_pos = 180;
   }
-  if (tilt.attached()) {
+  if (tilt_pos > tilt_max) {
+    Serial.print("Tilt overdrive: ");
+    Serial.print(tilt_pos);
+    Serial.print(">");
+    Serial.println(tilt_max);
+    tilt_pos = tilt_max;
+  }
+  if (pan.attached() && old_pan_pos != pan_pos) {
+    Serial.print("Moving pan to ");
+    Serial.print(pan_pos);
+    Serial.println("°");
+    pan.write(pan_pos);
+    old_pan_pos = pan_pos;
+  }
+  if (tilt.attached() && old_tilt_pos != tilt_pos) {
+    Serial.print("Moving tilt to ");
+    Serial.print(tilt_pos);
+    Serial.println("°");
     tilt.write(tilt_pos);
+    old_tilt_pos = tilt_pos;
   }
 }
 
